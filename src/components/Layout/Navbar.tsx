ac02e66230f155a16158f1e503a4bc7ee4a67318
AldairN <barsachiva34@gmail.com>
2024-04-30 22:04:35 -0600

    update

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import {FaHome, FaPlane, FaHotel } from "react-icons/fa";
 
 interface NavbarProps {
   Title: string;
-  image: string;
   Buttontext: string;
   HomeText: string;
   SearchText: string;
+  image: string;
 }
 export default function Navbar(props: NavbarProps) {
   return (
-    <div className="w-100">
+    <div className="w-100 ">
       <nav
         className="navbar navbar-expand-lg bg-dark border-bottom border-body "
         data-bs-theme="dark"
@@ -29,47 +30,52 @@ export default function Navbar(props: NavbarProps) {
             className="collapse navbar-expand-lg container-fluid navbar-collapse justify-content-center w-100 align-items-center "
             id="navbarText"
           >
+            <div className="collapse navbar-collapse" id="navbarScroll">
             <button
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#navbarText"
-              aria-controls="navbarText"
+              data-bs-target="#navbarScroll"
+              aria-controls="navbarScroll"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
             </button>
 
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center w-100 align-items-center ">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  {props.HomeText}
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-            </ul>
-          </div>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder={props.SearchText}
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+              <ul className="navbar-nav me-auto mb-2 navbar-nav-scroll mb-lg-0 justify-content-center w-100 align-items-center ">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    <FaHome />
+                    {props.HomeText}
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <FaPlane />
+                    Flights
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <FaHotel />
+                    {props.Buttontext}
+                  </a>
+                </li>
+              </ul>
+            </div>
+              </div>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder={props.SearchText}
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
         </div>
       </nav>
     </div>
