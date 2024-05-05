eab00c06b4460fd43feab9168443cde7265af1c7
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-05-05 13:31:46 -0600

    Adding props

--- src/components/Rafael/FrontPage.tsx
+++ src/components/Rafael/FrontPage.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import styles from '@/components/styles/FrontPage.module.css';
 
-export default function FrontPage() {
+interface FrontPageProps{
+  title:string;
+  description:string;
+  image:string;
+}
+
+export default function FrontPage({title,description,image}:FrontPageProps) {
   return (
     <div>
-       <section className={`p-3 mt-3 d-flex flex-column justify-content-center ${styles.frontPageImage}`}>
-            <h1 className="display-3 text-white position-relative">AirMAC</h1>
-            <h2 className="lead text-white position-relative">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
-        </section>
+      <section className={`p-3 d-flex flex-column justify-content-center ${styles.frontPageImage}`} style={{backgroundImage: `url('/IMG/${image}')`}}>
+        <h1 className="display-3 text-white position-relative">{title}</h1>
+        <h2 className="lead text-white position-relative">{description}</h2>
+      </section>
     </div>
   )
 }
